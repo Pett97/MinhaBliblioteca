ffd2b53949627bba03b28c0095927e6f1300831a
Pett97 <henriquepeterson138@gmail.com>
2025-01-30 00:09:21 -0300

    controller autor ok

--- back-bliblioteca/app/Http/Controllers/UserController.php
+++ back-bliblioteca/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -29,5 +30,26 @@ class UserController extends Controller
             'email' => $user->email,
             'token' => $token->plainTextToken
         ]]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json(['message' => "Não Foi encontrado nenhum usuario com esse email"]);
+        }
+
+        if ($user && !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'senha não confere']);
+        }
+
+        $token = $user->createToken($user->name);
+        return response()->json(['message' => "login efetuado com sucesso", "user" => $user, "token" => $token->plainTextToken]);
     }
 }
--- back-bliblioteca/routes/api.php
+++ back-bliblioteca/routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\AutorController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
     Route::post('create', [UserController::class, 'create']);
+    Route::post('login', [UserController::class, 'login']);
 });
+
+Route::apiResource('autor', AutorController::class);
