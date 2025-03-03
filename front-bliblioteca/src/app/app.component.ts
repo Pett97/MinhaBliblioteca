6f85a8d3ae3bc4c6b37759a27d49cc1a09bca1a0
Pett97 <henriquepeterson138@gmail.com>
2025-03-02 21:48:30 -0300

    login part1

--- front-bliblioteca/src/app/app.component.ts
+++ front-bliblioteca/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterModule],
+  imports: [RouterOutlet,RouterModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
